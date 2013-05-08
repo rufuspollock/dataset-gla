76ee4d5a57f988ce580f33fc5ec93bf467a3556d
Rufus Pollock <rufus.pollock@okfn.org>
2013-05-08 18:26:06 +0100

    [scripts/clean.sh][s]: use curl rather than wget.

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,2 +1,2 @@
-wget http://static.london.gov.uk/gla/expenditure/docs/2012-13-P11-250.csv
-tail -n +7 2012-13-P11-250.csv | head -n -4 | sed "s/^,//g" > 2013-jan.csv
+curl "http://static.london.gov.uk/gla/expenditure/docs/2012-13-P11-250.csv" | \
+  tail -n +7  | head -n -4 | sed "s/^,//g" > cache/2013-jan.csv
