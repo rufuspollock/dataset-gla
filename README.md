2929c698c7f4eb3ccaf7a63cb995b03914f7bd75
Rufus Pollock <rufus.pollock@okfn.org>
2013-07-16 19:36:22 +0100

    [misc][l]: partially working scraper (~50% of files parsed) plus details of all the problems with the data (old: 10d).

--- README.md
+++ README.md
@@ -6,6 +6,17 @@ See also this blog post <http://schoolofdata.org/2013/03/26/using-sql-for-lightw
 
 ## Data
 
+There are ~ 3 headings types (with additional variations and with various columns or not)
+
+Vendor ID,Vendor Name,Cost Element,Expenditure Account Code Description,SAP Document No,Amount £,Clearing Date
+
+Variations:
+
+* Vendor ID,Vendor Name,Cost Element,Expenditure Account Code Description,SAP Document No,Amount £,Clearing Date
+
+
+## Data Preparation
+
 CSV files are listed on <http://www.london.gov.uk/mayor-assembly/gla/spending-money-wisely/budget-expenditure-charges/expenditure-over-250>
 
 That site states:
@@ -14,33 +25,24 @@ That site states:
 > 
 > From Period 4 2012/13 onwards the report includes expenditure from the GLA's subsidiary, GLA Land & Property Ltd.
 
-There are 61 CSV files as of March 2013 (a list can be found in scrape.json").
+There are more than 60 CSV files as of July 2013 (a list can be found in scrape.json").
 
 Unfortunately the "format" varies substantially, not only in terms of fields but in e.g. number of blank columns or blank lines etc etc.
 
-* 2008 (Apr-Dec): Monthly files, amount over £1000
-  * Fields: Supplier, Expense, Amount
-  * Note no date so we just know month of expense not precise date
-* 2009 (Jan-Dec): Monthly files, amount over £1000
-  * Fields: Supplier, Expense, Amount (though Jan 2009 has 2 extra fields one with PLIN and the other with 
-  * Note no date so we just know month of expense not precise date
-* 2010: Jan is monthlay then starting in Feb 2010 move to what appears to be approx 28d periods (13 in a year)
-  * From Period 4 (June) onwards amounts are over £500 (prior to that over £1000)
-  * Fields: they vary from month to month in a non-systematic manner
-  * A usual set are: Vendor Name,Expenditure Account Code,Expenditure Account Code Description,"SAP Document No","Amount �",Clearing Date
-  * 5 "metadata" lines at the top of the file, one blank leading column
-* 2012 and 2013:
-  * Fields: Vendor ID,Vendor Name,Cost Element,Expenditure Account Code Description,Document No,Amount,Clearing Date
-  * 5 "metadata" lines at the top of the file, one blank leading column
+A summary can be found in this [Data Explorer gist][de-gist].
+
+[de-gist]: http://explorer.okfnlabs.org/#rgrp/63e14589091a917d175a/view/grid
 
 Aside: from the presence of "SAP Document No" field in several of the CSVs it appears likely that the GLA are using SAP for their accounting systems.
 
 ### Errors
 
-* Bad file for [Period 8 2012/13 (13 October - 10 November)](http://www.london.gov.uk/sites/default/files/Mayor's%20250%20Report%20-%202012-13%20-%20P8%20%20-%20Final.csv).  The file is not named in the usual way "Mayor's%20250%20Report%20-%202012-13%20-%20P8%20%20-%20Final.csv" and appears to be an Excel file that was not converted to CSV!
+* (July 2013) [Period 8 2012/13](http://static.london.gov.uk/gla/expenditure/docs/2012-13-P8-250.csv) is an HTML file showing a 403 Access Denied from someone's login session
+  * <del>(March 2013) Bad file for [Period 8 2012/13 (13 October - 10 November)](http://www.london.gov.uk/sites/default/files/Mayor's%20250%20Report%20-%202012-13%20-%20P8%20%20-%20Final.csv).  The file is not named in the usual way "Mayor's%20250%20Report%20-%202012-13%20-%20P8%20%20-%20Final.csv" and appears to be an Excel file that was not converted to CSV!</del>
 * Amounts are formatted with "," making them appear as strings to computers.
 * Dates vary substantially in format from "16 Mar 2011" in [this file](http://static.london.gov.uk/gla/expenditure/docs/2010-11-P13-500.csv) to "21.01.2010" in [January 2010 data](http://legacy.london.gov.uk/gla/expenditure/docs/january_2010.csv)
 * Use of (978) to indicate negative amounts rather than -978
+* Repeated data in [2012-13-P4 file](http://datapipes.okfnlabs.org/csv/html/?url=http://static.london.gov.uk/gla/expenditure/docs/2012-13-P4-250.csv#L870)
 
 ## Plan
 
--- scripts/process.js
+++ scripts/process.js
@@ -1,4 +1,156 @@
-var csv = require('csv');
+var csv = require('csv')
+  , fs = require('fs') 
+  , path = require('path')
+  ;
+
+var headings = [
+  'Vendor ID',
+  'Vendor Name',
+  'Cost Element',
+  'Expenditure Account Code Description',
+  'Document No',
+  'Amount',
+  'Date'
+  ]
+var info = JSON.parse(fs.readFileSync('scrape.json'));
+
+function main() {
+  var indir = 'archive/latest';
+  var outdir = 'tmp/clean';
+  // info.sources = info.sources.slice(1,20);
+  info.sources.forEach(function(url) {
+    var name = path.basename(url);
+    var src = path.join(indir, name);
+    var dest = path.join(outdir, name);
+    if (name == '2012-13-P8-250.csv') {
+      //  bad data (html file rather than csv!)
+      return;
+    }
+    console.log(name);
+    csv()
+      .from(src)
+      .to(dest)
+      .transform(function(row, idx) {
+        if (idx == 0) {
+          return headings;
+        }
+        var mapfunc = fixer[name];
+        if (!mapfunc) {
+          return [];
+        }
+        try {
+          return mapfunc(row, idx, name);
+        } catch(e) {
+          console.log(e);
+          console.log(row);
+          console.log(name + ':' + idx);
+        }
+      })
+      .on('error', function(e) {
+        console.error('*** ERROR with ' + name);
+        console.error(e);
+      })
+      ;
+  });
+}
+
+var fixer = {
+  'Mayor%27s%20250%20Report%20-%202013-14%20-%20P1%20%20-%20Combined.csv': std,
+  '2012-13-P13-250.csv': std,
+  '2012-13-P12-250.csv': std,
+  '2012-13-P11-250.csv': std,
+  '2012-13-P10-250.csv': std,
+  '2012-13-P9-250.csv': std,
+  '2012-13-P8-250.csv': std,
+  '2012-13-P7-250.csv': std,
+  '2012-13-P6-250.csv': std,
+  '2012-13-P5-250.csv': std,
+  '2012-13-P4-250.csv': std,
+  '2012-13-P3-250.csv': std,
+  '2012-13-P2-250.csv': std,
+  '2012-13-P1-500.csv': std,
+  '2011-12-P13-500.csv': std,
+  '2011-12-P12-500.csv': std,
+  '2011-12-P11-500.csv': std,
+  'Mayors500Report-2011-12-P10-Final.csv': std,
+  'Mayors-500-report2011-12-P9.csv': std,
+  '2011-12-P8-500.csv': std,
+  '2011-12-P7-500.csv': std,
+  '2011-12-P6-500.csv': std,
+  '2011-12-P5-500.csv': std,
+  '2011-12-P4-500.csv': std,
+  '2011-12-P3-500.csv': std,
+  '2011-12-P2-500.csv': std,
+  '2011-12-P1-500.csv': std,
+  '2010-11-P13-500.csv': std,
+  '2010-11-P12-500.csv': std,
+  '2010-11-P11-500.csv': std,
+  '2010-11-P10-500.csv': std,
+  '2010-11-P09-500.csv': std,
+  '2010-11-P08-500.csv': std,
+  '2010-11-P07-500.csv': std,
+  '2010-11-P06-500.csv': std,
+  '2010-11-P05-500.csv': std,
+  '2010-11-P04-500.csv': std,
+  '2010-11-P03.csv': std,
+  '2010-11-P02.csv': std,
+  '2010-11-P01.csv': std,
+  'P13-2009-10.csv': std,
+  'P12- 2009_10_FINAL.csv': std,
+  'january_2010.csv': std,
+  'december_2009.csv': std,
+  'november_2009.csv': std,
+  'october_2009.csv': std,
+  'september_2009.csv': std,
+  'august_2009.csv': std,
+  'july_2009.csv': std,
+  'june_2009.csv': std,
+  'may_2009.csv': std,
+  'april_2009.csv': std,
+  'march_2009.csv': std,
+  'february_2009.csv': std,
+  'january_2009.csv': std,
+  'december_2008.csv': std,
+  'november_2008.csv': std,
+  'october_2008.csv': std,
+  'september_2008.csv': std,
+  'august_2008.csv': std,
+  'july_2008.csv': std,
+  'june_2008.csv': std,
+  'may_2008.csv': std,
+  'april_2008.csv': std
+}
+
+function std(row, idx, name) {
+  // skip rows until the header row
+  // startline info extracted separately
+  if (idx < info.startline[name]) return null;
+
+  // special cases!!
+  // yes believe it or not this file repeats a whole bunch of data at the end of the file
+  if (name == '2012-13-P4-250.csv') {
+    if (idx > 865) return null;
+  }
+
+  while(row[0] == '') {
+    row = row.slice(1);
+  }
+  if (row[0]==='' || row.length < 3 || row[1] == '') return null;
+
+  return fixFields(row);
+}
+
+function fixFields(row) {
+  // fix amount field
+  row[5] = row[5].replace(/,/g, '');
+  // -ve amounts. (978) => -978
+  if (row[5].indexOf('(') != -1) {
+    row[5] = '-' + row[5].replace('(', '').replace(')', '');
+  }
+  // fix date field
+  row[6] = new Date(row[6]).toISOString().slice(0,10);
+  return row;
+}
 
 function fixUp() {
   csv()
@@ -19,4 +171,5 @@ function fixUp() {
   ;
 }
 
-fixUp();
+// fixUp();
+main();
