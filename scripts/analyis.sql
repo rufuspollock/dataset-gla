ef29bb430e09752ed77156cbb7d9b6d69c6d5388
Rufus Pollock <rufus.pollock@okfn.org>
2013-08-17 18:25:53 +0100

    [scripts][s]: update and modify analysis scripts plus add comments to headings.sh script.

--- scripts/analyis.sql
+++ scripts/analyis.sql
@@ -1,24 +1,33 @@
 .mode column
 .header ON
 .width 35
--- basic sum
-SELECT "Vendor Name", SUM(Amount) FROM data GROUP BY "Vendor Name" ORDER BY sum(amount) DESC LIMIT 20;
--- sum with average
-SELECT "Vendor Name", SUM(Amount), AVG(Amount), COUNT(*) FROM data GROUP BY "Vendor Name" ORDER BY sum(amount) DESC LIMIT 5;
 
-SELECT "Vendor Name", SUM(Amount) FROM data
-  WHERE "Vendor Name" NOT LIKE "%Borough%"
-  GROUP BY "Vendor Name"
+-- total
+SELECT SUM(Amount) FROM consolidated;
+
+-- total vendors
+SELECT COUNT(DISTINCT "Vendor") FROM consolidated;
+
+-- by month
+SELECT SUM(amount), strftime("%Y-%m", Date) FROM consolidated GROUP BY strftime("%Y-%m", Date);
+
+-- basic sum
+SELECT "Vendor", SUM(Amount) FROM consolidated GROUP BY "Vendor" ORDER BY sum(amount) DESC LIMIT 20;
+
+-- sum with average
+SELECT "Vendor", SUM(Amount), AVG(Amount), COUNT(*) FROM consolidated GROUP BY "Vendor" ORDER BY sum(amount) DESC LIMIT 5;
+
+-- by  vendor w/o boroughs
+SELECT "Vendor", SUM(Amount) FROM consolidated
+  WHERE "Vendor" NOT LIKE "%Borough%"
+  GROUP BY "Vendor"
   ORDER BY sum(amount) DESC
   LIMIT 10;
 
-SELECT "Vendor Name", SUM(Amount) FROM data
-  WHERE ("Vendor Name" NOT LIKE "%BOROUGH%" AND "Vendor Name" NOT LIKE "%HOUSING%")
-  GROUP BY "Vendor Name"
+-- by  vendor w/o boroughs and housing
+SELECT "Vendor", SUM(Amount) FROM consolidated
+  WHERE ("Vendor" NOT LIKE "%BOROUGH%" AND "Vendor" NOT LIKE "%HOUSING%")
+  GROUP BY "Vendor"
   ORDER BY sum(amount) DESC
   LIMIT 20;
-
-SELECT COUNT(DISTINCT "Vendor Name") FROM data;
-
-SELECT SUM(Amount) FROM data;
 
--- scripts/headings.sh
+++ scripts/headings.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
+#
+# Extract the CSV headings as best we can from the dumped files into a single
+# file for analysis
+
 grep -rn ",\"\?Amount" archive/latest/ | sort | sed 's/archive\/latest\///g' > tmp/headings.txt 
 echo "" >> tmp/headings.txt
 echo "# Not found" >> tmp/headings.txt
 grep -rnL ",\"\?Amount" archive/latest/ | sort | sed 's/archive\/latest\///g' >> tmp/headings.txt 
+
